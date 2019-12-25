a9ee76ce6dce5ace059ea8f90d52151fe5eb6154
PavloPantus <mr.pantus@gmail.com>
2019-12-25 16:50:26 +0200

    Updates

---
+++ precache-manifest.ba835d8b62a4a5e69ce1d762a713e2aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36bd86f4d1c48abf30d7dd4629e3c27",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "dc115ab7af71ef1c579f",
+    "url": "/react_tabs-with-router/static/css/main.e9a6c2fb.chunk.css"
+  },
+  {
+    "revision": "23370007f798e4455d85",
+    "url": "/react_tabs-with-router/static/js/2.f353167d.chunk.js"
+  },
+  {
+    "revision": "dc115ab7af71ef1c579f",
+    "url": "/react_tabs-with-router/static/js/main.6785e94c.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
